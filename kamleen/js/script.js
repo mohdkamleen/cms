d34cef67f8776a82c6b36f531fdbd7ada783bf73
mohdkamleen <kamleenmohd@gmail.com>
2021-09-15 21:19:06 +0530

    settingMainChild

--- kamleen/js/script.js
+++ kamleen/js/script.js
@@ -3,7 +3,7 @@ var copyright = document.getElementsByClassName("copyright")[0];
 var imageBack = document.getElementsByClassName("image-back")[0]; 
 var goToBottom = document.getElementsByClassName("go-to-down")[0]; 
 var logo = document.getElementsByClassName("logo")[0]; 
-var accupation = document.getElementsByClassName("accupation")[0]; 
+var accupation = document.getElementsByClassName("accupation")[0];  
 var nav = document.getElementsByTagName("nav")[0];
 var li1 = document.getElementsByTagName("li")[0];
 var li2 = document.getElementsByTagName("li")[1];
@@ -11,6 +11,8 @@ var li3 = document.getElementsByTagName("li")[2];
 var li4 = document.getElementsByTagName("li")[3];
 var myImage = document.getElementById("myImage"); 
 var effect8 = document.getElementById("effect8");  
+var settingMain = document.getElementById("settingMain");  
+var settingMainChild = document.getElementById("settingMainChild");  
 
 document.body.onresize = (e) => {
     if(e.target.innerWidth > 900){
@@ -31,6 +33,16 @@ hamburger.addEventListener("click", () =>{
          hamburger.classList.replace("fa-bars","fa-times"); 
     } else {
         hamburger.classList.replace("fa-times","fa-bars");
+    }
+})
+
+settingMain.addEventListener("click", () =>{
+    settingMainChild.classList.toggle("active");  
+
+    if(settingMain.className === "fa fa-cogs"){
+        settingMain.classList.replace("fa-cogs","fa-times"); 
+    } else {
+        settingMain.classList.replace("fa-times","fa-cogs");
     }
 })
 
